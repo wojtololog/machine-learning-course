193c298e403e3e2f5d2467a56c7042b4e0dec1f8
wojtololog <wojtololog@gmail.com>
2023-12-14 16:49:01 +0100

    Project initial structure

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 import sys
+import pandas as pd
+import numpy as np
+
+from src.tasks.TasksSelector import TasksSelector
+
+
+def read_data():
+    pd.set_option("display.max.columns", 100)
+    data_path = "https://raw.githubusercontent.com/Yorko/mlcourse.ai/main/data/"
+    data = pd.read_csv(data_path + "adult.data.csv")
+    print(data.head())
+    return data
+    pass
 
 
 def main() -> int:
+    task_number = 1
+    dataset = read_data()
+    tasks_selector = TasksSelector(dataset, task_number)
     return 0
 
 
---
+++ src/tasks/TasksSelector.py
@@ -0,0 +1,10 @@
+class TasksSelector:
+    def __init__(self, dataset, task_number):
+        self.dataset = dataset
+        self.task_number = task_number
+
+    def run(self):
+        match self.task_number:
+            case 1:
+                pass
+        pass
